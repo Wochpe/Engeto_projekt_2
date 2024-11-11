1550593b2ffb284099dd60415c729f85ddf2f343
Wochpe <144677150+Wochpe@users.noreply.github.com>
2024-11-11 12:28:17 +0100

    Final Bulls_cows.py

--- Bulls_cows.py
+++ Bulls_cows.py
@@ -7,14 +7,15 @@ discord: karolinamach
 """
 from random import randint, choice
 
+
+# uživatelské funkce a oddělovač
 delim = "-" * 40
 
 def search_for_duplicates(number: str) -> bool:
     '''
-    Verifies presence of duplicates in a number: str.
-    Returns duplicates: bool
-    >>> True for presence at least one duplicite number.
-    >>> False for presence only unique numbers.
+    Verifies presence of duplicates in an inputed number(str).
+    Returns:
+        duplicates(bool): True when present at least one duplicate number.
     '''
     num_list = []
     for digit in number:
@@ -27,6 +28,12 @@ def search_for_duplicates(number: str) -> bool:
     return duplicates
 
 def find_bulls_cows(number: str) -> int:
+    '''
+    Counts the number of correcty guessed numbers.
+    Returns:
+        bulls(int): The count of all correctly guessed numbers on correct position.
+        cows(int): The count of remaining correctly guessed numbers on wrong position.
+    '''
     bulls = 0
     cows = 0
     for cipher1, cipher2 in zip(number, win_number):
@@ -46,7 +53,7 @@ print('''I've generated a random 4 digit number for you."
 "Let's play a bulls and cows game.''')
 print(delim)
 
-# generování náhodného 4 místného čísla abcd, které nezačíná nulou
+# generování náhodného 4 místného čísla abcd, které nezačíná nulou a je bez duplicit
 all_nubers = []
 x = 0
 while x < 10:
@@ -67,24 +74,24 @@ win_number = str(a) + str(b) + str(c) +str(d)
 print("Enter a number:")
 print(delim)
 n_bulls = 0
-n_try = 1
+n_try = 0
 while n_bulls < 4:
     player_num = input(">>> ")
-    if not player_num.isnumeric():
+    if not player_num.isnumeric():                          # Ověření číselného vstupu
         print("Incorect input. All symbols must be numeric.")
         print(delim)
-    elif len(player_num) != 4:
+    elif len(player_num) != 4:                              # Ověření délky vstupu
         print("Incorect input lenght. The input has to contain exactly 4 numbers.")
         print(delim)
-    elif search_for_duplicates(player_num):
+    elif search_for_duplicates(player_num):                         # Ověření jedinečnosti čísel
         print("Incorect input. All 4 numbers must be unique.")
         print(delim)
-    elif player_num[0] == "0":
+    elif player_num[0] == "0":                          # Ověření, že nula není na první pozici
         print("Incorect input. First number can't be 0.")
         print(delim)   
     else:
         n_bulls, n_cows = find_bulls_cows(player_num)
-        if n_bulls == 1:
+        if n_bulls == 1:                            # Podmínky sloužící pouze k vypisování správného tvaru slova bull a cow jejich podle počtu.
             bull_ = "bull"
         else:
             bull_ = "bulls"
@@ -94,7 +101,7 @@ while n_bulls < 4:
             cow_ = "cows"
         print(n_bulls, bull_ + ",", n_cows, cow_)
         print(delim)
-        n_try += 1
+        n_try += 1                          # Počítá platně zadané pokusy.
 
 else:
     print("Correct, you've guessed the right number\nin", n_try, "guesses!")
